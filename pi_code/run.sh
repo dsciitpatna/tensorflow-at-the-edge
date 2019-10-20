52b83afdcd273b39c447bc49b7bb96b4b905bc9d
sheetalg <sheetalgupta00001@gmail.com>
2019-10-20 15:41:48 +0530

    add comments to run.sh

--- pi_code/run.sh
+++ pi_code/run.sh
@@ -1,4 +1,11 @@
+# record video using pi_camera
 python3 video_pi.py
+
+# convert video from .h264 to .mp4 for processing
 ffmpeg -framerate 24 -i video.h264 -c copy video.mp4
+
+# send the video to gcp virtual machine
 gcloud compute scp ./video.mp4 instance-1:~/program/program/input
+
+# finally remove the video
 rm video.mp4
